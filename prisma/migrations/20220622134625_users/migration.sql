e64577552f5e517add2ef1e14fa672c2cc9cc900
Ali Nuraldin <alirunwwc@gmail.com>
2023-04-05 17:01:14 +0200

    Remove prisma as codebase

--- prisma/migrations/20220622134625_users/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" VARCHAR(255) NOT NULL,
-    "publicId" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20220706131429_renamning_dao_overviews/migration.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM "Dao";
--- AlterTable
-ALTER TABLE "Dao" DROP COLUMN "overviewHtml",
-DROP COLUMN "tokenOverviewHtml",
-ADD COLUMN     "overview" TEXT NOT NULL,
-ADD COLUMN     "tokenOverview" TEXT NOT NULL;
-
--- AlterTable
-ALTER TABLE "Token" ADD COLUMN     "main" BOOLEAN NOT NULL DEFAULT false;
